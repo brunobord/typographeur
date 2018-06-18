aa4c7c7b1788fa65961576b15f36f96fb2d8b509
Bruno Bord <bruno@jehaisleprintemps.net>
2018-06-18 23:38:27 +0200

    Dropped useless fixture factory

--- tests/test_insecables.py
+++ tests/test_insecables.py
@@ -1,6 +1,23 @@
 import pytest
 from typographeur import typographeur
-from .fixtures_factory import _build_fixture_insecable
+
+
+fixtures_insecable = [
+    ("<p>exemple{mark}</p>", "<p>exemple&nbsp;{mark}</p>"),
+    ("<p>exemple {mark}</p>", "<p>exemple&nbsp;{mark}</p>"),
+    # insecable space below
+    ("<p>exemple {mark}</p>", "<p>exemple&nbsp;{mark}</p>"),
+    ("<p>exemple    {mark}</p>", "<p>exemple&nbsp;{mark}</p>"),
+    ("<p>exemple    {mark} hello</p>", "<p>exemple&nbsp;{mark} hello</p>"),
+    (
+        "<p>exemple{mark} hello {mark} here</p>",
+        "<p>exemple&nbsp;{mark} hello&nbsp;{mark} here</p>"),
+]
+
+
+def _build_fixture_insecable(mark):
+    for _input, expected in fixtures_insecable:
+        yield (_input.format(mark=mark), expected.format(mark=mark))
 
 
 @pytest.mark.parametrize("input,expected", _build_fixture_insecable(':'))
